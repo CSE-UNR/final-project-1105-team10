00739e59afc32380021db53cc7b38386aeb5368d
Amanda DeBurle <adeburle@CS135-adeburle.local>
2024-12-09 21:15:47 +0000

    Final draft

--- madlibA.c
+++ madlibA.c
@@ -2,18 +2,14 @@
 //Amanda's part
 
 #include <stdio.h>
-#include <stdbool.h>
 #define FILE_NAME "madlib1.txt"
 
 void userInputStorage(int row, int column, char madlib[][column]);
 void encodedStory();
-void readDecodedStory(const char *filename, char *buffer, int bufferSize);
-void displayStory(const char *story);
 
 int main() {
 	int row = 21, column = 150;
 	char madlib[row][column];
-	
 	printf("Enter an adjective: ");
 	scanf("%s", madlib[2]);
 	printf("Enter an adjective: ");
@@ -37,8 +33,6 @@ int main() {
 
 	userInputStorage(row, column, madlib);
 	encodedStory();
-	void readDecodedStory(filename, buffer, bufferSize);
-	void displayStory(story);
 
 return 0;
 }
@@ -74,34 +68,5 @@ return;
 		printf("%s", input);
 	}
 fclose(fp);
-}
-
-
-
-//Jordan Cadelina's Part
-
-
-// Read Decode 
-void readDecodedStory(const char *filename, char *buffer, int bufferSize) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Could not open file %s", filename);
-        return;
-    }
-
-    int i = 0;
-    char ch;
-    while ((ch = fgetc(file)) != EOF && i < bufferSize - 1) {
-        buffer[i++] = ch;
-    }
-    buffer[i] = '\0'; 
-
-    fclose(file);
-}
-
-// Display story
-void displayStory(const char *story) {
-    printf("Complete:\n\n");
-    printf("%s\n", story);
 }
 
