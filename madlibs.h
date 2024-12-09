61c51209fd00931f1c782b933b3c88e01614ebba
Jordan Cadelina <jcadelina@unr.edu>
2024-12-09 21:03:50 +0000

    Jordans Part

---
+++ madlibs.h
@@ -0,0 +1,9 @@
+#ifndef MADLIBS_H
+#define MADLIBS_H
+
+// Function prototypes
+void readDecodedStory(const char *filename, char *buffer, int bufferSize);
+void displayStory(const char *story);
+
+#endif
+
